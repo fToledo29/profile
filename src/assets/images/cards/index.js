9d12a588a45622d6301927f3bb59b9539255a2a6
Fernando Toledo <fernando_29837@live.com>
2020-05-20 23:35:57 -0500

    Adding images

---
+++ src/assets/images/cards/index.js
@@ -0,0 +1,7 @@
+// export const notification = require('./notification.png')
+export const niceImage = require('./9fb73a97063669.5ebc6ab5ea934.jpg');
+export const niceImage2 = require('./4883b694926993.5ebc2f17d37d4.jpg');
+export const niceImage3 = require('./f263d897125933.5ebdb75813b71.jpg');
+export const niceImage4 = require('./4af5ce96839423.5eb7a0de9b325.jpg');
+export const niceImage5 = require('./a1c47997056591.5ebc4a5db2016.jpg');
+export const niceImage6 = require('./ec6f2097157057.5ebe844a4186f.jpg');
