43904850158776b3c5722a8e90ec357ec8e77bd8
Fernando Toledo <fernando_29837@live.com>
2020-05-21 15:21:19 -0500

    Adding firebase

---
+++ public/precache-manifest.3ccfe5f48800f64fd416f43905d2ae3b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "552869006d0fbef0012b3ad6da69befc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07e14e1e5ac741ee9b2a",
+    "url": "/static/css/main.d51c820c.chunk.css"
+  },
+  {
+    "revision": "e5e76d26e099fd916eca",
+    "url": "/static/js/2.35dd7e2b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.35dd7e2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07e14e1e5ac741ee9b2a",
+    "url": "/static/js/main.32d2bcca.chunk.js"
+  },
+  {
+    "revision": "e29727336eee38de13a7",
+    "url": "/static/js/runtime-main.b59380e1.js"
+  },
+  {
+    "revision": "1b39ef33455f4b40d21f39da436793fb",
+    "url": "/static/media/4883b694926993.5ebc2f17d37d4.1b39ef33.jpg"
+  },
+  {
+    "revision": "4b4eeed6b8ced236dbdbd920ef3ea80f",
+    "url": "/static/media/4af5ce96839423.5eb7a0de9b325.4b4eeed6.jpg"
+  },
+  {
+    "revision": "b839cad0b92865aa0855e718cce4c510",
+    "url": "/static/media/9fb73a97063669.5ebc6ab5ea934.b839cad0.jpg"
+  },
+  {
+    "revision": "08e87fee287de74eacdba33692c4f143",
+    "url": "/static/media/a1c47997056591.5ebc4a5db2016.08e87fee.jpg"
+  },
+  {
+    "revision": "7e8047188467b9157903469d878bc327",
+    "url": "/static/media/ec6f2097157057.5ebe844a4186f.7e804718.jpg"
+  },
+  {
+    "revision": "60e1b1d8b8efe9ce716bd7ec0fdf95a4",
+    "url": "/static/media/f263d897125933.5ebdb75813b71.60e1b1d8.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
