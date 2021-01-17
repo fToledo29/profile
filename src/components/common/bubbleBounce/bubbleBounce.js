8111c25fe87b723568dbce1e1c6c9a78daf21b3a
Fernando Toledo <fernando_29837@live.com>
2021-01-17 17:34:12 -0600

    bubblebounce files created

---
+++ src/components/common/bubbleBounce/bubbleBounce.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BubbleBounce = () => {
+	return (
+		<div>
+			
+		</div>
+	)
+}
+
+export default BubbleBounce
--- src/components/me/me.js
+++ src/components/me/me.js
@@ -11,7 +11,7 @@ const Me = () => {
 		},
 		{
 			size: 400,
-			color: '#00ff80',
+			color: '#00eeff',
 		},
 		{
 			size: 700,
@@ -23,7 +23,7 @@ const Me = () => {
 		},
 		{
 			size: 900,
-			color: '#00ff80',
+			color: '#00eeff',
 		},
 		{
 			size: 200,
@@ -35,7 +35,7 @@ const Me = () => {
 		},
 		{
 			size: 400,
-			color: '#c300ff',
+			color: '#f5c542',
 		},
 		{
 			size: 400,
@@ -59,7 +59,7 @@ const Me = () => {
 		},
 		{
 			size: 400,
-			color: '#00ff80',
+			color: '#00eeff',
 		},
 		{
 			size: 500,
@@ -83,7 +83,7 @@ const Me = () => {
 		},
 		{
 			size: 400,
-			color: '#c300ff',
+			color: '#f5c542',
 		},
 		{
 			size: 100,
@@ -101,27 +101,33 @@ const Me = () => {
 			size: 400,
 			color: '#c300ff',
 		},
-		// {
-		// 	size: 400,
-		// 	color: '#00ff80',
-		// },
-		// {
-		// 	size: 400,
-		// 	color: '#00ff80',
-		// },
-		// {
-		// 	size: 700,
-		// 	color: '#ca6f9d',
-		// },
-		// {
-		// 	size: 800,
-		// 	color: '#c300ff',
-		// },
-		// {
-		// 	size: 900,
-		// 	color: '#00ff80',
-		// },
-	]
+		{
+			size: 400,
+			color: '#00ff80',
+		},
+		{
+			size: 400,
+			color: '#00eeff',
+		},
+		{
+			size: 700,
+			color: '#ca6f9d',
+		},
+		{
+			size: 800,
+			color: '#f5c542',
+		},
+		{
+			size: 900,
+			color: '#00ff80',
+		},
+	];
+	let animationIndex = 1;
+
+	const reandom = (max, min) => {
+		return Math.floor(Math.random() * (max - min + 1) + min);
+	}
+
 	return (
 		<>
 			<div className='my-profile-container'>
@@ -141,17 +147,13 @@ const Me = () => {
 			</div>
 
 			{bubbles.map((bubble, index) => {
-				const top = Math.floor(Math.random() * 100);
-				const left = Math.floor(Math.random() * 100);
-				const animationNum = Math.floor(Math.random() * 5);
+				const top = reandom(1, 100);
+				const left = reandom(1, 100);
+				const animationNum = reandom(1, 10);
+				animationIndex = animationIndex < 7 ? animationIndex + 1 : 1;
 				const animationClass = 'bubble-' + animationNum;
-
-				console.log('animationClass: ', animationClass);
-
-				const delay = Math.floor(Math.random() * 8000 * 8);
-
-				console.log('Delay: ', delay);
-
+				const delay = reandom(15000, 1000);
+				const alternate = reandom(1, 2);
 				let res = new Promise((resol, rej)=> {
 					setTimeout(() => {
 	
@@ -159,13 +161,17 @@ const Me = () => {
 						className={animationClass}
 						key={index} 
 						style={{
+							borderRadius: '50%',
 							backgroundColor: bubble.color,
 							height: (bubble.size / 4), 
 							width: (bubble.size / 4),
 							top: `${top}%`,
 							left: `${left}%`,
 							position: 'fixed',
-							opacity: 0.5
+							opacity: 0.5,
+							animation: `move-buble-${animationNum} ${alternate === 1 ? '30s' : '70s'} ease-out infinite`,
+							animationDirection: alternate === 1 ? 'alternate-reverse' : 'alternate',
+							WebkitAnimationDirection: alternate === 1 ? 'alternate-reverse' : 'alternate',
 						}} ></div>)
 					}, delay);
 				});
