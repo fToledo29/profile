f5f36119927901383e2f45d5f096e7d169347c3a
Fernando Toledo <fernando_29837@live.com>
2021-02-08 17:42:08 -0600

    Updating footer and background

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ import Home from './components/home/home';
 import { Router, Route, Switch } from "react-router-dom";
 import MyProfile from './components/me/me';
 import history from './shared/history';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons'
+library.add(fab);
+library.add(fas);
+library.add(far);
 
 function App() {
   return (
--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './footer.css';
-import imgx from '../../assets/images/footer/flowerFooter.jpg';   
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export const Footer = () => {
 
@@ -9,45 +9,109 @@ export const Footer = () => {
 	const githubUser = 'fToledo29';
 	const email = 'fernandotoledo23@gmail.com';
 	const instagramUser = 'ftoledo23';
+	const facebookUser = '29837T'; // TODO: don't commit
+
+	const [mediaTwitter, mediaTwitterChange] = useState('4x');
+	const [mediaGithub, mediaGithubChange] = useState('4x');
+	const [mediaEnvelop, mediaEnvelopChange] = useState('4x');
+	const [mediaLinkedIn, mediaLinkedInChange] = useState('4x');
+
+	const [mediaFacebook, mediaFacebookChange] = useState('4x');
+	const [mediaInstagram, mediaInstagramChange] = useState('4x');
 
 
 	const twitter = 'https://twitter.com/' + twitterUser;
 	const linkedIn = 'https://www.linkedin.com/in/' + linkedInUser;
 	const github = 'https://github.com/' + githubUser;
 	const instagram = 'https://www.instagram.com/' + instagramUser
+	const facebook = 'https://www.facebook.com/' + facebookUser;
 
 	return (
 		<div className="footer">
-			<img 
-			alt='Footer' 
-			src={imgx}/>
+			<div 
+			className="footer-bg"
+			alt='Footer'
+			/>
 			<div className='card-overlay'></div>
 			<div className='footer-info'>
 				<ul>
 					<li>
-						{twitter}
+
+						<a 
+						onMouseEnter={() => mediaTwitterChange('5x')}
+						onMouseLeave={() => mediaTwitterChange('4x')}
+						target="_blank" 
+						rel="noopener noreferrer" 
+						href={twitter}>
+
+							<FontAwesomeIcon 
+							size={mediaTwitter} className="icons" icon={['fab','twitter-square']} />
+						</a>
 					</li>
 					<li>
-						{github}
+						<a 
+						onMouseEnter={() => mediaGithubChange('5x')}
+						onMouseLeave={() => mediaGithubChange('4x')}
+						target="_blank" 
+						rel="noopener noreferrer" 
+						href={github}>
+							<FontAwesomeIcon 
+							size={mediaGithub} className="icons" icon={['fab','github-square']} />
+						</a>
 					</li>
 				</ul>
 			</div>
 			<div className="footer-media">
 				<ul>
 					<li>
-						{linkedIn}
+						<a 
+						onMouseEnter={() => mediaEnvelopChange('5x')}
+						onMouseLeave={() => mediaEnvelopChange('4x')}
+						target="_blank" 
+						rel="noopener noreferrer" 
+						href={'mailto:' + email}>
+							<FontAwesomeIcon 
+							size={mediaEnvelop} className="icons" icon={['far','envelope']} />
+						</a>
 					</li>
 					<li>
-						{email}
+						<a 
+						onMouseEnter={() => mediaLinkedInChange('5x')}
+						onMouseLeave={() => mediaLinkedInChange('4x')}
+						target="_blank" 
+						rel="noopener noreferrer" 
+						href={linkedIn}>
+							<FontAwesomeIcon 
+							size={mediaLinkedIn} className="icons" icon={['fab','linkedin']} />
+						</a>
 					</li>
 				</ul>
 			</div>
 			<div className="footer-contact">
 				<ul>
 					<li>
-						{instagram}
+						<a 
+						onMouseEnter={() => mediaFacebookChange('5x')}
+						onMouseLeave={() => mediaFacebookChange('4x')}
+						target="_blank" 
+						rel="noopener noreferrer" 
+						href={facebook}>
+						<FontAwesomeIcon 
+						 size={mediaFacebook} className="icons" icon={['fab','facebook-square']} />
+						</a>
 					</li>
-					<li>Facebook</li>
+					<li>
+						<a 
+						onMouseEnter={() => mediaInstagramChange('5x')}
+						onMouseLeave={() => mediaInstagramChange('4x')}
+						target="_blank" 
+						rel="noopener noreferrer" 
+						href={instagram}>
+							<FontAwesomeIcon 
+						 size={mediaInstagram} className="icons" icon={['fab','instagram-square']} />
+
+						</a>
+					</li>
 				</ul>
 			</div>
 		</div>
