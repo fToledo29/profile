4c297b8d768d3c692504f6d749f01ebbf789c925
Fernando Toledo <fernando_29837@live.com>
2021-02-09 23:09:11 -0600

    Updating header

--- src/components/header/menu-option/menu-option.js
+++ src/components/header/menu-option/menu-option.js
@@ -4,8 +4,8 @@ const MenuOption = ({ title, handleClick, imageUrl}) => {
 	return (
 		<li className='option' onClick={() => handleClick()}>
 			<h1 className='option-title'>{title}</h1>
-			<img className='option-image'
-			alt={title} src={imageUrl} />
+			{/* <img className='option-image'
+			alt={title} src={imageUrl} /> */}
 		</li>
 	);
 };
